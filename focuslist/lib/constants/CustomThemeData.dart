bb8004f59398f214f2410434c96d221ad3f927ce
Swagnik Das <104345028+swagthehooman@users.noreply.github.com>
2025-03-23 23:11:05 +0530

    themeExtension for global theme constants

--- focuslist/lib/constants/CustomThemeData.dart
+++ focuslist/lib/constants/CustomThemeData.dart
@@ -1,3 +1,46 @@
 import 'package:flutter/material.dart';
 
-class customthemedata {}
+abstract class AbstractTheme {
+  static const primaryLightGrey = Color(0x00e4ebf1);
+  static const primaryDarkGrey = Color(0x00b5bfc6);
+  static const primaryDarkerGrey = Color(0x006e7f8d);
+
+  static const primaryShadow = Color(0x00fafbff);
+  static const secondaryShadow = Color(0x00161b1d);
+}
+
+class AppColorsExtension extends ThemeExtension<AppColorsExtension> {
+  AppColorsExtension({
+    required this.primary,
+    required this.shadow,
+  });
+
+  final Color primary;
+  final Color shadow;
+
+  @override
+  ThemeExtension<AppColorsExtension> copyWith({
+    Color? primary,
+    Color? shadow,
+  }) {
+    return AppColorsExtension(
+      primary: primary ?? this.primary,
+      shadow: shadow ?? this.shadow,
+    );
+  }
+
+  @override
+  ThemeExtension<AppColorsExtension> lerp(
+    covariant ThemeExtension<AppColorsExtension>? other,
+    double t,
+  ) {
+    if (other is! AppColorsExtension) {
+      return this;
+    }
+
+    return AppColorsExtension(
+      primary: Color.lerp(primary, other.primary, t)!,
+      shadow: Color.lerp(shadow, other.shadow, t)!,
+    );
+  }
+}
